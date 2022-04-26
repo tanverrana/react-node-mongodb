3c8ec74ffc90b94908f1b6942bfdd37643886482
tanverrana <tanversobur@gmail.com>
2022-04-26 20:29:16 +0600

    show data in client side

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/user")
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    });
+    const handleUserDelete = id => {
+        const proceed = window.confirm("Are you sure you want to delete?");
+        if (proceed) {
+            console.log("Deleting user with id", id);
+        }
+
+    }
     return (
         <div>
-            <h2>This is our home page.</h2>
+            <h2>Available Users:{users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}>
+                        {user.name} :: {user.email}
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
