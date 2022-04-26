bdfdb5b6f49e7b3eb1d6e0e738440c3d005eba82
tanverrana <tanversobur@gmail.com>
2022-04-26 11:44:37 +0600

    data pass local host 5000

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AddUser from './components/AddUser/AddUser';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
         <Route path="/user/add" element={<AddUser></AddUser>}></Route>
       </Routes>
     </div>
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 const AddUser = () => {
+    const handleAddUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = { name, email };
+
+        //send data to the server
+        fetch("http://localhost:5000/user", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("success", data)
+            })
+    }
     return (
         <div>
-            <h2>Add User Page</h2>
+            <h2>Please Add User!</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder="Name" required />
+                <input type="email" name="email" placeholder="Email" required /><br />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };
